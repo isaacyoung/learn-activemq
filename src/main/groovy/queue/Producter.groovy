eb1900e3796e861271762a73e1a275a454964678
isaac <isaacyoung82@163.com>
2018-02-04 14:24:21 +0800

    init

---
+++ src/main/groovy/queue/Producter.groovy
@@ -0,0 +1,36 @@
+package queue
+
+import org.apache.activemq.ActiveMQConnectionFactory
+import org.apache.activemq.command.ActiveMQQueue
+
+import javax.jms.Session
+
+def url = "tcp://192.168.31.145:61616"
+def name = "queue-test"
+
+// 创建ConnectionFactory
+def connectionFactory = new ActiveMQConnectionFactory(url)
+
+// 创建Connection
+connection = connectionFactory.createConnection()
+
+// 创建目的地
+destination = new ActiveMQQueue(name)
+
+// 创建会话
+session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+
+// 创建生产者
+producer = session.createProducer(destination)
+
+// 文本消息
+message = session.createTextMessage()
+
+for (i in 1..100) {
+    message.setText("This is message $i")
+    println("Sending message: " + message.getText())
+    producer.send(message)
+}
+
+// 关闭连接
+connection.close()
